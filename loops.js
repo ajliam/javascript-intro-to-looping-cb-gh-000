8656c4220e90a79b9749501ebd8495b6e16133fe
Aj Liam <andrewjordan35@gmail.com>
2020-06-22 23:32:53 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+//  Define a function called doWhileLoop in loops.js. The function should take an integer as an argument. Use the incrementVariable() function (you can copy it from this README) in the condition, and console log "I run once regardless." while incrementVariable() returns a number less than the parameter received. (Your condition might look something like incrementVariable() < num.) Remember that it should also console log when receiving 0 as a parameter because the do-while runs before the condition is checked.
+
+
+
+function forLoop(){
+
+  for (let i = 1; i < 25; i++); 
+    console.log("I am ${i} 1 strange loop.");
+  }
+
