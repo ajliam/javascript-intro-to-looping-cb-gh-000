a1c7acbc3e5925d212392457268df94125760d5f
Aj Liam <andrewjordan35@gmail.com>
2020-06-22 23:41:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@
 
 function forLoop(){
 
-  for (let i === 0; i < 25; i++);{}
-    console.log("I am ${i} strange loops.");
+  for (let i = 0; i < 25; i++);{
+    console.log(`I am ${i} strange loops.`);
   }
 }
